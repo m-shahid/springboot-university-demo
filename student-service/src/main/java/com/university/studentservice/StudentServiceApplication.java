2a650e09307c59f652dc7dcbf957e242966a4f02
m-shahid <mshahidsayyad@gmail.com>
2022-01-26 19:29:24 +0530

    added eureka server for service discovery (#1)

--- student-service/src/main/java/com/university/studentservice/StudentServiceApplication.java
+++ student-service/src/main/java/com/university/studentservice/StudentServiceApplication.java
@@ -3,6 +3,7 @@ package com.university.studentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.university.entity")
 @EnableJpaRepositories("com.university.repository")
 @EnableFeignClients("com.university.feignclient")
+@EnableEurekaClient
 public class StudentServiceApplication {
 
     public static void main(String[] args) {
